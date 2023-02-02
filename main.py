564834b110da8107f0dd04621f8b0ea2cdaf281a
Tomoe-495 <hasnainsiddique495@gmail.com>
2023-02-02 13:40:15 +0500

    main.py file setup

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+
+W, H = 1000, 400
+FPS = 60
+
+pygame.init()
+clock = pygame.time.Clock()
+
+win = pygame.display.set_mode((W, H), 0, 32)
+pygame.display.set_caption("Survival")
+
+
+def main():
+    run = True
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                sys.exit()
+
+
+if __name__ == "__main__":
+    main()
