0d431af9cbad6af2b3eefabc43f59f7d6927c8c3
Tomoe-495 <hasnainsiddique495@gmail.com>
2023-02-03 11:59:15 +0500

    added new rects generating from the json file in "map" folder
    now to work on getting the images

--- tilemap.py
+++ tilemap.py
@@ -1,10 +1,15 @@
 import pygame
+import json
 from main import W, H
 
 def get_map(name):
     with open(name, 'r') as f:
         f = f.read()
         return f.split('\n')
+
+def get_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
 
 def screen_check(rect, scroll):
     if rect.x - scroll[0] < -rect.width:
@@ -21,24 +26,27 @@ class Tiledmap:
     def __init__(self):
         self.color = (255,255,255)
         self.map = get_map("map.txt")
+        self.game_map = get_json('map/forest.json')
         self.tiles = []
-        self.size = 32
+        self.size = self.game_map["levels"][0]["layerInstances"][0]["__gridSize"]
         self.scroll = [0, 0]
+
+        self.csvMAP = self.game_map["levels"][0]["layerInstances"][0]
 
         x = 0
         y = 0
-        for block in self.map:
-            for b in block:
-                if b == "x":
-                    self.tiles.append(pygame.Rect(x, y, self.size, self.size))
-                x += self.size
-            y += self.size
-            x = 0
-    
+        for block in self.csvMAP["intGridCsv"]:
+            if block == 1:
+                self.tiles.append(pygame.Rect(x, y, self.size, self.size))
+            x += self.size
+
+            if x == self.size * self.csvMAP["__cWid"]:
+                y += self.size
+                x = 0
+
     def draw(self, win):
         for tile in self.tiles:
-            if screen_check(tile, self.scroll):
-                pygame.draw.rect(win, self.color, (tile.x - self.scroll[0], tile.y - self.scroll[1], tile.width, tile.height))
+            pygame.draw.rect(win, self.color, (tile.x - self.scroll[0], tile.y - self.scroll[1], tile.width, tile.height))
 
     def camera(self, pl):
         speed = 10
